5cf9f9bce9fcb893b85afefa82d8a241589b37f3
daly <tu_email@example.com>
2026-02-09 20:42:36 +0100

    ly

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.querySelectorAll('a[href^="#"]').forEach(link => {
-    link.addEventListener("click", e => {
-      e.preventDefault();
-      document
-        .querySelector(link.getAttribute("href"))
-        .scrollIntoView({ behavior: "smooth" });
-    });
+  link.addEventListener("click", e => {
+    e.preventDefault();
+    document
+      .querySelector(link.getAttribute("href"))
+      .scrollIntoView({ behavior: "smooth" });
   });
+});
